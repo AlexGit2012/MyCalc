bb4273cbb11390183c18eb523b5d92832e3185b3
AlexGit2012 <a.karavaychik@gmail.com>
2020-09-29 23:35:24 +0300

    feat: add sqrt and pow functions

--- script.js
+++ script.js
@@ -9,58 +9,133 @@ class Calculator {
         this.previousValue = "";
         this.currentValue = "";
         this.operation = undefined;
+        this.readyForReset = false;
+    }
+
+    getDisplayNumber(number) {
+        if ((number === "Error: Negative value") || (number === "Error: division by zero")) {
+            this.readyForReset = true;
+            return number;
+        }
+        if (number === "-") return "-";
+
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split(".")[0]);
+        const decimalDigits = stringNumber.split(".")[1];
+        let integerDisplay;
+        if (isNaN(integerDigits)) {
+            integerDisplay = ""
+        } else {
+            integerDisplay = integerDigits.toLocaleString("en", {
+                maximumFractionDigits: 0
+            });
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
     }
 
     update() {
-        this.previousOperandTextElement.innerText = this.previousValue;
-        this.currentOperandTextElement.innerText = this.currentValue;
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText =
+                `${this.getDisplayNumber(this.previousValue)} ${this.operation}`
+        } else this.previousOperandTextElement.innerText = this.previousValue
+
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentValue);
     }
 
     delete() {
-
+        this.currentValue = this.currentValue.toString().slice(0, -1);
     }
 
     appendNumber(number) {
+        if (this.readyForReset === true) this.clear();
         if (number === '.' && this.currentValue.includes('.')) return
         this.currentValue = this.currentValue.toString() + number.toString();
     }
 
     chooseOperation(operation) {
+        if ((this.currentValue === "Error: Negative value") || (this.currentValue === "Error: division by zero")) return
+        if (this.currentValue === "-") return
+        if (this.currentValue === "" && operation == "-") {
+            this.currentValue = '-' + this.currentValue;
+            return
+        }
+
         if (this.currentValue === "") return
         if (this.previousValue !== '') {
-            this.compute()
+            this.compute();
         }
         this.operation = operation;
         this.previousValue = this.currentValue;
         this.currentValue = "";
+        this.readyForReset = false;
     }
 
     compute() {
         let computeResult;
-        const prev = parseFloat(this.previousValue)
-        const curr = parseFloat(this.currentValue)
+
+        let prevDecima;
+        let currDecima;
+        if (this.previousValue.toString().includes(".")) {
+            prevDecima = this.previousValue.toString().split(".")[1].length;
+        } else prevDecima = 0;
+        if (this.currentValue.toString().includes(".")) {
+            currDecima = this.currentValue.toString().split(".")[1].length;
+        } else currDecima = 0;
+        let maxDecima = (prevDecima > currDecima ? prevDecima : currDecima);
+
+        const prev = parseFloat(this.previousValue);
+        console.log(prev)
+        const curr = parseFloat(this.currentValue);
+        console.log(curr)
+
         if (isNaN(prev) || isNaN(curr)) return
         switch (this.operation) {
             case "+":
-                computeResult = prev + curr
+                computeResult = prev * (10 ** maxDecima) + curr * (10 ** maxDecima)
                 break
             case "-":
-                computeResult = prev - curr
+                computeResult = prev * (10 ** maxDecima) - curr * (10 ** maxDecima)
                 break
             case "*":
-                computeResult = prev * curr
+                computeResult = (prev * (10 ** maxDecima) * curr * (10 ** maxDecima)) / (10 ** maxDecima)
                 break
             case "/":
-                computeResult = prev / curr
+                if (curr==0) {
+                    computeResult = "Error: division by zero"
+                    break
+                }
+                computeResult = (prev * (10 ** maxDecima) / curr * (10 ** maxDecima)) / (10 ** maxDecima)
+                break
+            case "^":
+                computeResult = (prev ** curr) * (10 ** maxDecima)
                 break
             default:
                 return
         }
-        this.currentValue = computeResult;
+        this.currentValue = isNaN(computeResult) ? computeResult : computeResult / (10 ** maxDecima)
         this.operation = undefined;
         this.previousValue = "";
+        this.readyForReset = true;
     }
 
+    sqrt() {
+        if (this.currentValue === "") return;
+        if (parseFloat(this.currentValue) < 0) {
+            this.currentValue = "Error: Negative value";
+            this.previousValue = "";
+            this.operation = undefined;
+            this.readyForReset = true;
+            return;
+        }
+        this.currentValue = Math.sqrt(this.currentValue);
+        this.previousValue = "";
+        this.operation = undefined;
+        this.readyForReset = true;
+    }
 
 }
 
@@ -71,10 +146,11 @@ const deleteButton = document.querySelector('[data-delete]');
 const equalButton = document.querySelector('[data-equal]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const sqrtButton = document.querySelector('[data-sqrt]');
 
 const calculator = new Calculator(currentOperandTextElement, previousOperandTextElement);
 
-console.log(currentOperandTextElement)
+console.log(sqrtButton)
 
 numbersButtons.forEach(button => {
     button.addEventListener('click', () => {
@@ -94,3 +170,19 @@ equalButton.addEventListener('click', button => {
     calculator.compute();
     calculator.update();
 })
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.update();
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.update();
+})
+
+sqrtButton.addEventListener('click', button => {
+    calculator.sqrt();
+    calculator.update();
+})
+
